26687ad01e1cc81d4e19fbd7034657814b9acb21
TiokengSamuel <samueledward40@gmail.com>
2020-10-22 17:38:33 +0100

    home

--- src/pages/HomePageComponent.js
+++ src/pages/HomePageComponent.js
@@ -4,7 +4,10 @@ class Home extends React.Component {
     render() {
         return(
             <div>
-                <h1></h1>
+                <h1>HomePage</h1>
+                <p>
+                    Some content
+                </p>
             </div>
         )
     }
