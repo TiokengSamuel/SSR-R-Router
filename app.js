cb85c7ab8810456a52e7386fc2dee3902b4b482b
TiokengSamuel <samueledward40@gmail.com>
2020-10-22 17:45:13 +0100

    app

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import Home from './src/pages/HomePageComponent'
 import {Switch, Route} from 'react-router'
 
 class App extends React.Component {
     render() {
         return(
-
+            <Route path="/example" render={props => (
+                
+            )}/>
         )
     }
 }
