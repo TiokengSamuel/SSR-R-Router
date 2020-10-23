430be36937088d3a4844343463ad133b01fec0a6
TiokengSamuel <samueledward40@gmail.com>
2020-10-23 02:40:41 +0100

    get

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.length('*', (req, res) => {
+app.get('*', (req, res) => {
     res.send('This is my working app')
 })
 
