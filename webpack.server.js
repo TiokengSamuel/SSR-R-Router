0f7ff2c8528857271ae1dec8043698f5b997792b
TiokengSamuel <samueledward40@gmail.com>
2020-10-23 02:32:26 +0100

    web

--- webpack.server.js
+++ webpack.server.js
@@ -1,3 +1,10 @@
+const path = require('path')
+
 moduele.exports = {
     target: 'node',
+    entry: './server.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    }
 }
