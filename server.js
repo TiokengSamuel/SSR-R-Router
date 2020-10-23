c0522be0432965cff8547fb1c6591b1707adfa3e
TiokengSamuel <samueledward40@gmail.com>
2020-10-23 02:40:16 +0100

    solving cannot GET

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.length('*', (req, res) => {
+    res.send('This is my working app')
+})
+
 app.listen(PORT, () => {
     console.log(`App running ${PORT}`);
 });
